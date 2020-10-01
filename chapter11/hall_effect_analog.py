e3d1e2838412132ddb7e3fd5fc6801875ec0131e
Gary Smart <gary@smart-itc.com.au>
2020-10-01 23:59:17 +1000

    Upload

--- chapter11/hall_effect_analog.py
+++ chapter11/hall_effect_analog.py
@@ -28,8 +28,25 @@ analog_channel_A0 = AnalogIn(ads, ADS.P0)  # ADS.P0 --> A0
 if __name__ == '__main__':
       
     try:
+        # Sample a resting voltage to calculate delta voltage.
+        print("Calibrating... make sure magnet is not near hall effect sensor")
+
+        resting_volts = 0
+        sum_volts = 0
+
+        for n in range(0, 100):
+            # Take 100 samples
+            sum_volts = sum_volts + analog_channel_A0.voltage
+
+        resting_volts = sum_volts / 100
+
         while True:
-            output = "Volts={:>5.3f}, Value={}".format(analog_channel_A0.voltage, analog_channel_A0.value)
+            volts = analog_channel_A0.voltage
+            volts_delta = volts - resting_volts
+
+            # Analog input voltage will fluctuate a little. Using 1 decimal point
+            # to steady display output.
+            output = "Volts={:>5.1f}, Delta Volts={:>5.1f}".format(volts, volts_delta)
             print(output)
             sleep(0.05)
 
