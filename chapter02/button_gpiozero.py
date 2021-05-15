8877ad2fb3293d71adb89444a4f0547822781c3e
garyss2000 <81270232+garyss2000@users.noreply.github.com>
2021-05-15 10:44:57 +1000

    Updates

--- chapter02/button_gpiozero.py
+++ chapter02/button_gpiozero.py
@@ -8,9 +8,9 @@ Dependencies:
 
 Built and tested with Python 3.7 on Raspberry Pi 4 Model B
 """
-from gpiozero import Device, LED, Button                                        # (1)
+from gpiozero import Device, LED, Button                                                 # (1)
 from gpiozero.pins.pigpio import PiGPIOFactory
-import signal                                                                   # (2)
+import signal                                                                            # (2)
 
 LED_GPIO_PIN = 21
 BUTTON_GPIO_PIN = 23
@@ -18,16 +18,17 @@ BUTTON_GPIO_PIN = 23
 Device.pin_factory = PiGPIOFactory() #set gpiozero to use pigpio by default.
 
 def pressed():
-    led.toggle()                                                                # (3)
-    state = 'on' if led.value == 1 else 'off'                                   # (4)
-    print("Button pressed: LED is " + state)                                    # (5)
+    led.toggle()                                                                         # (3)
+    state = 'on' if led.value == 1 else 'off'                                            # (4)
+    print("Button pressed: LED is " + state)                                             # (5)
 
 led = LED(LED_GPIO_PIN)
 led.off()
 
-button = Button(BUTTON_GPIO_PIN, pull_up=True, bounce_time=0.1)                 # (6)
-button.when_pressed = pressed                                                   # (7)
+
+button = Button(BUTTON_GPIO_PIN, pull_up=True, bounce_time=0.1) # Bounce time in seconds # (6)
+button.when_pressed = pressed                                                            # (7)
 
 print("Press button to turn LED on and off.")
 
-signal.pause() # Stops program from exiting.                                    # (8)
+signal.pause() # Stops program from exiting.                                             # (8)
--- errata/README.md
+++ errata/README.md
@@ -26,3 +26,23 @@ This error does not affect a readers ability to successfully run the example, bu
 The following image illustrates the issue.
 
 ![Code Example](./Chapter2Page72.png)
+
+### Page 64 - Responding to a button press with PiGPIO
+
+Under the heading **Button pin configuration**, we see a snippet of code from the file `chapter02/button_gpiozero.py`
+
+At line 3 `set_glitch_filter` has the parameter 10000, which is the debounce time measured in __microseconds__.
+
+The proceeding paragraph states the value is in __milliseconds__, which is incorrect.
+
+![Code Example](./Chapter2Page65.png)
+
+In this example, the timeout is therefore 10000 / 1000000 = 0.01 seconds
+
+This difference in timing value is not expected to affect the reader's ability to complete the exercise. However, should the example behave erratically when the button is pressed, the reader should change the parameter at line 3 from `10000` to `100000` to make the debounce time 0.1 seconds. The corrected line will therefore be:
+
+```
+pi.set_glitch_filter(BUTTON_GPIO_PIN, 100000)         # (3)
+```
+
+Also note that earlier in the chapter on Page 63, we see a code example from file `chapter02/button_gpiozero.py` using a debounce time of 0.1 seconds. The error on Page 64 and in file `chapter02/button_gpiozero.py` mean that the GPIOZero and PiGPIO examples are not strictly identical, although from an illustrative perspective for the purposes of the chapter they can be considered functionally identical.
